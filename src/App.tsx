44f4e4c7b46e5ae6ac52fc4f716e6ac8d06e93ba
Ambar-Ghaffar <ambaraarain@gmail.com>
2024-06-25 19:25:37 +0500

    updated

--- src/App.tsx
+++ src/App.tsx
@@ -3,10 +3,7 @@ import React from 'react';
 import Dashboard from './components/Dashboard';
 
 const App: React.FC = () => {
-  // return <Dashboard />;
-  return(
-    <div>abcccc</div>
-  )
+  return <Dashboard />;
 };
 
 export default App;
